777cda54a8edc8a67d7426e912251f31e3741f39
shubhamsm <shubhammehraxc@gmail.com>
2020-08-20 23:53:50 +0530

    hello

---
+++ django-social-network/ReadMe.txt
@@ -0,0 +1,36 @@
+Aman Bhai
+
+//app 
+
+CDN + Domain + Hosting
+shubhammehraxc-hello
+
+
+
+stack:
+    django
+    Front: React     
+
+
+Apps:
+    Main:- SocialNetwor
+    Accounts:- All accounts emails and all
+    UserProfile: To check and edit profile of the user 
+
+    NewsFeed
+        
+    Media: MediaFiles
+
+
+Models:
+    User
+
+
+
+
+
+
+
+
+
+
---
+++ django-social-network/socialnetwork/routing.py
@@ -0,0 +1,16 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from friends import routing as friends_routing
+from notifications import routing as notifications_routing
+from communications import routing as communications_routing
+
+
+
+# message sent will be showed in notification 
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            friends_routing.websocket_urlpatterns + notifications_routing.websocket_urlpatterns + communications_routing.websocket_urlpatterns
+        )),
+})
